62e992e6c8c64eb86fe7be2ac6343bd45867fd49
daulen <daulenz@gmail.com>
2021-04-11 17:04:44 -0500

    completed assigment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Write your solution here!
-const cats=["Milo", "Otis", "Garfield"];
+
+
+const cats= ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name){
-return cats.push(name)
+    return cats.push(name)
 }
 
 function destructivelyPrependCat(name){
@@ -13,21 +15,26 @@ function destructivelyRemoveLastCat(){
     return cats.pop()
 }
 
+
 function destructivelyRemoveFirstCat(){
     return cats.shift()
 }
 
 function appendCat(name){
-return [ ...cats,name]
+return [...cats,name]
+
 }
 function prependCat(name){
-  
     return [name,...cats]
 }
+
 function removeLastCat(){
-    return cats.slice(0,cats.length-1)
+return cats.slice(0,cats.length-1)
+
 }
 
 function removeFirstCat(){
     return cats.slice(1)
 }
+
+
